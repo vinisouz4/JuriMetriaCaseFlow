0e3e3c7f49f856a952c9a6fc0b53746110b75b12
vinisouz4 <vini_wow16@hotmail.com>
2025-02-25 23:03:52 -0300

    Pegando clientes do escavador

--- test.py
+++ test.py
@@ -4,65 +4,15 @@ from src.adapter.dataframe.pandas.index import PandasDataFrame
 # # from src.adapter.dataframe.polars.index import PolarsDataFrame
 from src.api.escavador.escavador import apiEscavador
 from src.use_case.dataCaseFlow.readData import ReadCaseFlowData
-from src.use_case.dataDataJud.readDataJud import ReadDataJud
-
-
-
-# from src.api.apiDataJud.getAPIDataJud import ReadDataJud
-# from src.adapter.core.config import Settings
-
-# import json
-# import asyncio
-
-# from src.use_case.insights.insights import Insights
-# from src.use_case.tribunais.tribunais import getTribunalNumber
-# from src.utils.utils import Utils
-
-# teste = ReadDataJud()
-
-# settings = Settings()
-
-# insights = Insights(PandasDataFrame())
-
-# datajud = ReadDataJud()
-
-# dataJud = getDataJud()
-
-# testeSupa = ReadCaseFlowData(PandasDataFrame())
-
-# util = Utils()
-
-# df = testeSupa.getData("Case", "true", True)
-
-# dataJud.saveData(df)
-
-# # Retirar isso depois da aprovacao do projeto
-# df = df[df["client_id"].isin([13, 30, 1248]) & df["uf"].isin(["SP"])]
-
-# df["endpoint_tribunal"] = df["tribunal_justica"].apply(getTribunalNumber)
-
-# with open(f"./src/data/lake/data_{util.getToday()}.json", "w") as f:
-#     for i in df["endpoint_tribunal"].unique():
-#         print(i)
-#         listNumbers = df[df["endpoint_tribunal"] == i]["external_number"].tolist()
-#         request = asyncio.run(datajud.getData(
-#             url = settings.API_PUBLI_DATAJUD, 
-#             processNumber=listNumbers, 
-#             headers={
-#                 "Authorization": settings.API_PUBLI_DATAJUD_KEY,
-#                 "Content-Type": "application/json"
-#             },
-#             numberEndPoint=i
-#         ))
-        
-#         json.dump(request, f, indent=4, ensure_ascii=False)
-
-
-# from src.use_case.dataDataJud.readDataJud import ReadDataJud
+from src.use_case.dataEscavador.readDataEscavador import ReadEscavador
 
 escavador = apiEscavador()
 
 case = ReadCaseFlowData(PandasDataFrame())
+
+
+# getDataJud.saveData(teste)
+
 
 df = case.getData("Case", "true", True)
 
@@ -70,7 +20,7 @@ teste = escavador.formatListProcess(df)
 
 print(teste[:10])
 
-# escavador.saveData(teste[:10])
+escavador.saveData(teste[:10])
 # atualizacao = getDataJud()
 # atualizacao.saveData(df)
 
